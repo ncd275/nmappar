7a6523e86583bbf8c0571b3fecef11c52fd1d2bd
Nicholas Dangles <nicholasdangles@gmail.com>
2017-07-25 23:41:49 -0500

    added port search and partial regex

--- nmappar.py
+++ nmappar.py
@@ -1,6 +1,7 @@
 import click
 import re
 
+
 @click.command()
 @click.option('--options', default="hps", help='number of greetings')
 @click.option('--function', default="null", help='number of greetings')
@@ -20,6 +21,16 @@ def main(options, function, file):
     if 'uniq-services' in function.lower():
         printBanner(["p","s"])
         uniqServices(opts, data)
+    if 'search-port' in function.lower():
+        portStr = function.split("=")[1]
+        ports = []
+        if ',' in portStr:
+            ports = portStr.split(",")
+        else:
+            ports.append(portStr)
+
+        printBanner(opts)
+        searchPorts(opts, ports, data)
 
 def printFile(data):
     click.echo(data)
@@ -58,7 +69,7 @@ def getLiveHosts(opts, data):
 
             for l in line.split("Ports: ")[1].split(", "):
                 if '/open/' in l:
-                    pair = (line.split("Host: ")[1].split(" ()")[0],l.split('/open/tcp//')[0],l.split('/open/tcp//')[1])
+                    pair = (line.split("Host: ")[1].split(" ()")[0],re.split("/open/[^\s]*?//", l)[0],re.split("/open/[^\s]*?//", l)[1])
                     list1.append(pair)
     printFunction(list1, opts)
 def searchServices(opts, searchTerm, data):
@@ -68,9 +79,27 @@ def searchServices(opts, searchTerm, data):
 
             for l in line.split("Ports: ")[1].split(", "):
                 if '/open/' in l:
-                    pair = (line.split("Host: ")[1].split(" ()")[0],l.split('/open/tcp//')[0],l.split('/open/tcp//')[1])
+                    pair = (line.split("Host: ")[1].split(" ()")[0],re.split("/open/[^\s]*?//", l)[0],re.split("/open/[^\s]*?//", l)[1])
                     if searchTerm.lower() in pair[2].lower():
                         list1.append(pair)
+    printFunction(list1, opts)
+
+def searchPorts(opts, ports, data):
+    list1 = []
+    pair = ''
+    for line in data:
+        if 'Host:'in line and 'Ports:' in line:
+
+            for l in line.split("Ports: ")[1].split(", "):
+                if '/open/' in l:
+                    if 'Ignored State' in l:
+                        pair = (line.split("Host: ")[1].split(" ()")[0],re.split("/open/[^\s]*?//", l)[0],re.split("/open/[^\s]*?//", l)[1].split("\tIgnored State:")[0])
+                    else:
+                        pair = (line.split("Host: ")[1].split(" ()")[0],re.split("/open/[^\s]*?//", l)[0],re.split("/open/[^\s]*?//", l)[1])
+
+                    for port in ports:
+                        if port.lower() == pair[1].lower():
+                            list1.append(pair)
     printFunction(list1, opts)
 
 def uniqServices(opts, data):
